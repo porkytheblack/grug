47937d3d7a2c7f98430eef7d67f66df4e4c7a8ac
porkytheblack <porkytheblack@gmail.com>
2023-04-01 08:43:20 +0000

    feat: added chat prompter

---
+++ apps/chat-4/index.ts
@@ -0,0 +1,80 @@
+import readline from "node:readline"
+import { stdin as input, stdout as output } from 'node:process';
+import { Configuration, OpenAIApi } from "openai";
+import chalk from "chalk";
+import dotenv from "dotenv"
+dotenv.config({
+    path: ".env"
+})
+
+console.log(process.env.TZ)
+
+const config = new Configuration({
+    apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+
+const customReadline = (question: string): Promise<{
+    rl: readline.Interface,
+    answer: string
+}> => new Promise((res, rej)=>{
+    const rl = readline.createInterface({
+        input,
+        output
+    })
+    try {
+        rl.question(question, (answer)=>{
+            res({
+                answer,
+                rl
+            })
+        })
+    } catch (e) {
+        rej(e)
+    }
+})
+
+
+const theGptStuff = (the_question: string): Promise<{answer?: string}> => new Promise((res, rej)=>{
+    openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `
+           Anser this question::
+           ${the_question}
+        `,
+        max_tokens: 300,
+        n: 1,
+        temperature: 0.5,
+    }).then(({
+        data: {
+            choices
+        }
+    })=>{
+        res({
+            answer: choices[0].text
+        })
+    }).catch((e)=>{
+        rej(e)
+    })
+})
+
+
+
+const promptMachine = async () => {
+    console.log("Hey from prompter 1.0.0\n")
+    await customReadline("You Fuck Face::").then(({answer, rl})=>{
+        rl.close()
+        theGptStuff(answer).then(({answer})=>{
+            console.log("GPT-4::", chalk.blue(answer))
+            promptMachine()
+        }).catch((e)=>{
+            console.log(chalk.red("Hey Something did't go well\n"))
+            promptMachine()
+        })
+    })
+}
+
+promptMachine()
+
